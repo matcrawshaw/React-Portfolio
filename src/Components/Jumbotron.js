6e8faba749e20002985632481d0c1255a3958c9b
Tay Crawshaw <tayc9518@gmail.com>
2023-04-08 14:23:14 +0100

    amended logo

--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -90,7 +90,7 @@ function HomeJumbotron() {
     <div className={classes.root}>
       <Container size="lg" style={{ display: "inline", height: rem(1000) }}> 
   <code style={{color: theme.white, display: "block"}} >Hi there 👋 i'm</code> 
-  <h1 style={{color: "#96F2D7", fontFamily: "Jaldi-Bold", display: "block"}}>Mat Crawshaw </h1>
+  <h1 style={{color: "#96F2D7", fontFamily: "Jaldi-Bold", display: "block"}}>Tay Crawshaw </h1>
   <Avatar  style={{background: "rgba(0, 0, 0, 0.6)", opacity: "90%", display: "inline-block", marginTop: -10 }} stylesrc={avatarPhoto} alt="it's me" />
   <code style={{color: theme.white, marginTop: 20, display: "block"}} >And i'm a front-end web developer. </code> 
   <Link to="/work" end>
